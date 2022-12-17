99b1e3557ccc0ad9ab617976e71d5e6a17c56515
motionbyfabi <fabiodinota@gmail.com>
2022-12-17 18:33:26 +0100

    zindex fix

--- components/home/index.jsx
+++ components/home/index.jsx
@@ -33,7 +33,7 @@ const index = () => {
 
         </div> */}
         <div className="relative w-[90%] h-[800px] overflow-hidden rounded-2xl"> {/* container for whole meats section */}
-        <div className="w-full h-full absolute z-[9999] flex justify-center items-center flex-col">
+        <div className="w-full h-full absolute z-[100] flex justify-center items-center flex-col">
             <div className="relative w-[300px] h-[180px] lg:w-[550px] lg:h-[300px]">
               <Image src={Wordmark} alt="wordmark" fill />
             </div>
